5fb688da847a2a17777577424b565e1d1b936f9e
Ana Santoandre <anasantoandre84@gmail.com>
2024-04-30 15:15:17 -0300

    empecé la función ordenar numeros

---
+++ src/EjerciciosJavaIntro/OrdenarNumeros.java
@@ -0,0 +1,24 @@
+package EjerciciosJavaIntro;
+
+import java.util.Scanner;
+
+public class OrdenarNumeros {
+    public static void ingresarNumeros(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("ingrese un número");
+        int numero1 = leer.nextInt();
+        System.out.println("ingrese un número");
+        int numero2 = leer.nextInt();
+        System.out.println("ingrese un número");
+        int numero3 = leer.nextInt();
+
+        if(numero1 > numero2 && numero1 > numero3){
+            System.out.println("el numero más grande es " + numero1);
+        } else if (numero2 > numero1 && numero2 > numero3) {
+            System.out.println("el numero más grande es " + numero2);
+        }
+        else {
+            System.out.println("el numero mas grande es " + numero3);
+        }
+    };
+};
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import Calculadora.MenuCalculadora;
 import EjerciciosJavaIntro.EjercicioConvertirMoneda;
 import EjerciciosJavaIntro.EjercicioIMC;
 import EjerciciosJavaIntro.ValidarClave;
+import EjerciciosJavaIntro.OrdenarNumeros;
 public class Main {
     public static void main(String[] args) {
         Scanner leerConsola = new Scanner(System.in);
         //MenuCalculadora.menuCalculadora();
         //EjercicioIMC.ingresarInfoCorporal();
         //EjercicioConvertirMoneda.ingresarMontoPesos();
-        ValidarClave.ingresoClave();
+        //ValidarClave.ingresoClave();
+        OrdenarNumeros.ingresarNumeros();
     };
 };
