7f2e627f74dc4f30cb0f022e1b3e85759730cdaa
Ana Santoandre <anasantoandre84@gmail.com>
2024-04-30 13:57:22 -0300

    modifiqué mensaje de ingreo de contraseña

--- src/EjerciciosJavaIntro/ValidarClave.java
+++ src/EjerciciosJavaIntro/ValidarClave.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class ValidarClave {
     public static void ingresoClave(){
         Scanner leer = new Scanner(System.in);
-        System.out.println("ingresa una contraseña");
+        System.out.println("ingresa una contraseña mayor a 8 dígitos");
         String clave = leer.nextLine();
         System.out.println("ingresala nuevamente");
         String claveRepetida = leer.nextLine();
